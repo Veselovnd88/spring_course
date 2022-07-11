70a9c4d1304532b25a3a1be46693fab73d5ee88c
nikolay <veselovnd@gmail.com>
2022-07-11 22:18:53 +0300

    урок 5 dependency injection

--- spring-app1/src/main/java/ru/veselov/springcourse/TestSpring.java
+++ spring-app1/src/main/java/ru/veselov/springcourse/TestSpring.java
@@ -1,4 +1,8 @@
 package ru.veselov.springcourse;
+//урок 5
+//dependency injection - создаются классы - будущие бины
+//создаются зависимости с помощью спринг -аннотации, хml или код
+//все объекты берутся из контейнера спринга
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 //внедрение через конструктор
@@ -8,13 +12,14 @@ public class TestSpring {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");//создается контекст
-		Music music = context.getBean("musicBean",Music.class);//внедрение зависимости
+		//Music music = context.getBean("musicBean",Music.class);//внедрение зависимости
 		//объект создается не в классе, а передается, поэтому в xml constructor-arg не надо.
-		//если бы создавался внутри класса - то передавали как параметр конструктора
-		//часть 6 - dependeny injection -> через сеттер
-		MusicPlayer mp = new MusicPlayer(music);
-		mp.playMusic();
+		//бин мьюзик внедряется вручную
+		//MusicPlayer mp = new MusicPlayer(music);//внедрение вручную
 		
+		MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);//внедрение зависимости через
+		//xml
+		musicPlayer.playMusic();
 		context.close();
 	}
 }
