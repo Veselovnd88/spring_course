d6081eb595825cbb05e1bacc214fb342dc8b7a5e
nikolay <veselovnd@gmail.com>
2022-07-15 19:11:28 +0300

    lesson 13 task+task

--- spring-app1/src/main/java/ru/veselov/springcourse/MusicPlayer.java
+++ spring-app1/src/main/java/ru/veselov/springcourse/MusicPlayer.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 //@Component
 public class MusicPlayer {
